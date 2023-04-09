ef48dd5f8c9ff8820c77bd4a4d7d12f5d8c673f9
tuagkcuf <jokersms222@gmail.com>
2023-04-09 19:28:32 +0300

    add box for version changing

---
+++ contracts/Box.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.8; 
+
+contract Box {
+    uint256 internal value;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
+
+    // Stores a new value in the contract
+    function store(uint256 newValue) public {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Reads the last sotred value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+
+    // Increments the stored value by one
+    function increment() public {
+        value += 1;
+        emit ValueChanged(value);
+    }
+
+    function version() public pure returns (uint256) {
+        return 1;
+    }
+}
