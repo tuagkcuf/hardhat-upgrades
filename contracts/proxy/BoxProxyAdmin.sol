c8e7a6be7fcf830268e22327e9887ca9a5e825a5
tuagkcuf <jokersms222@gmail.com>
2023-04-10 15:19:14 +0300

    add script for upgrade

--- contracts/proxy/BoxProxyAdmin.sol
+++ contracts/proxy/BoxProxyAdmin.sol
@@ -1,10 +1,13 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 contract BoxProxyAdmin is ProxyAdmin {
-    constructor(address /* owner */) ProxyAdmin() {
-        
+    constructor(
+        address /* owner */
+    ) ProxyAdmin() {
+        // We just need this for our hardhat tooling right now
     }
 }
